7054ad4aaff5c45c06b2d70241615c3c435d4ac3
Maulin Patel <53448446+MaulinPatel@users.noreply.github.com>
2020-02-03 21:19:03 -0500

    Add files via upload

---
+++ Controllers/ApplicationControllers/instructorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Car_Rental.Models;
+
+namespace Car_Rental.Controllers.ApplicationModel
+{
+    public class instructorsController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: instructors
+        public ActionResult Index()
+        {
+            return View(db.instructors.ToList());
+        }
+
+        // GET: instructors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            instructor instructor = db.instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // GET: instructors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: instructors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "inst_id,inst_name,inst_charge,inst_speciality,inst_car,inst_gender")] instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.instructors.Add(instructor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(instructor);
+        }
+
+        // GET: instructors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            instructor instructor = db.instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // POST: instructors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "inst_id,inst_name,inst_charge,inst_speciality,inst_car,inst_gender")] instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(instructor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(instructor);
+        }
+
+        // GET: instructors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            instructor instructor = db.instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // POST: instructors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            instructor instructor = db.instructors.Find(id);
+            db.instructors.Remove(instructor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/ApplicationControllers/mylessonController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Car_Rental.Models;
+
+namespace Car_Rental.Controllers.ApplicationControllers
+{
+    public class mylessonController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: mylesson
+        public ActionResult Index()
+        {
+            return View(db.my_lesson.ToList());
+        }
+
+        // GET: mylesson/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            my_lesson my_lesson = db.my_lesson.Find(id);
+            if (my_lesson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(my_lesson);
+        }
+
+        // GET: mylesson/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: mylesson/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "user_id,user_name,inst_id,lesson_days,lesson_time")] my_lesson my_lesson)
+        {
+            if (ModelState.IsValid)
+            {
+                db.my_lesson.Add(my_lesson);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(my_lesson);
+        }
+
+        // GET: mylesson/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            my_lesson my_lesson = db.my_lesson.Find(id);
+            if (my_lesson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(my_lesson);
+        }
+
+        // POST: mylesson/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "user_id,user_name,inst_id,lesson_days,lesson_time")] my_lesson my_lesson)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(my_lesson).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(my_lesson);
+        }
+
+        // GET: mylesson/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            my_lesson my_lesson = db.my_lesson.Find(id);
+            if (my_lesson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(my_lesson);
+        }
+
+        // POST: mylesson/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            my_lesson my_lesson = db.my_lesson.Find(id);
+            db.my_lesson.Remove(my_lesson);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
